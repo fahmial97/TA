cc0c68c3fc4c30bf0f131fedd1006bbe9d4296bc
fahmial97 <48953068+fahmial97@users.noreply.github.com>
2019-03-27 16:19:09 +0700

    menghapus fungsi tidak berguna

--- application/controllers/Ruang.php
+++ application/controllers/Ruang.php
@@ -91,23 +91,5 @@ class Ruang extends CI_Controller
 
    }
 
-   private function _uploadImage()
-{
-    $config['upload_path']          = './asset/img/ruang/';
-    $config['allowed_types']        = 'gif|jpg|png';
-    $config['file_name']            = $this->id;
-    $config['overwrite']		      	= true;
-    $config['max_size']             = 1024; // 1MB
-    // $config['max_width']            = 1024;
-    // $config['max_height']           = 768;
-
-    $this->load->library('upload', $config);
-
-    if ($this->upload->do_upload('image')) {
-        return $this->upload->data("file_name");
-    }
-
-    return "default.jpg";
-}
 
 }
